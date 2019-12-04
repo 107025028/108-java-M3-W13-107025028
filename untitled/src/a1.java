0fb09fe8c0d4c7ae735525051b339cfd2e4dab4c
107025028 <58501280+107025028@users.noreply.github.com>
2019-12-04 20:25:24 +0800

    Update a1.java

--- untitled/src/a1.java
+++ untitled/src/a1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class a1 {
+public class M3Q1 {
     public static void main(String args[]){
         Scanner s=new Scanner(System.in);
         char daty[]=s.nextLine().toCharArray();
