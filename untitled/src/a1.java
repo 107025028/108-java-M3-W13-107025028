02e7023695bbf03bdceca375f56c23c29ce780bb
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-12-04 15:53:32 +0800

    2019-12-4

---
+++ untitled/src/a1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class a1 {
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        char daty[]=s.nextLine().toCharArray();
+        for (int i=daty.length-1;i>=0;i--){
+            System.out.print(daty[i]);
+        }
+    }
+}
---
+++ untitled/src/a2.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class a2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Random re = new Random();
+        re.setSeed(23323456);
+        int da[] = new int[6];
+        boolean a = true;
+        int i = 0;
+        while (i < 6) {
+            a = true;
+            da[i] = re.nextInt(42) + 1;
+            for (int j = 0; j < i; j++) {
+                if (da[j] == da[i]) {
+                    a = false;
+                    break;
+                }
+                if (a == true) {
+                    System.out.print(da[i]);
+                    i++;
+
+                }
+            }
+
+        }
+    }
+}
